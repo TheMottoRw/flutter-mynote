66695082fab155df52772f414a82b3cf066b5b54
Manzi Roger <hasua.mr@gmail.com>
2024-04-13 11:50:00 +0200

    added firebase auth abstraction

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordFoundAuthException implements Exception {}
+
+// register exceptions
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+class InvalidCredentialAuthException implements Exception {}
+
+//generic exceptions
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
