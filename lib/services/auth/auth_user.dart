b55a8d3e00e5b157c7cf4698f742eaddf948ab36
Manzi Roger <hasua.mr@gmail.com>
2024-04-13 22:50:04 +0200

    astep-10 test not all working

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -2,8 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuth show User;
 import 'package:flutter/foundation.dart';
 
 @immutable
-class AuthUser{
+class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(FirebaseAuth.User user) => AuthUser(user.emailVerified);
+
+  const AuthUser({required this.isEmailVerified});
+
+  factory AuthUser.fromFirebase(FirebaseAuth.User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
